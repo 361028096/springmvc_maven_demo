62e219b4357c48e2cf76af627e9e8816ac9726dd
wei.he <wei.he@bkjk.com>
2018-01-10 15:43:12 +0800

    update

---
+++ src/test/java/com/run/demo/test.java
@@ -0,0 +1,11 @@
+package com.run.demo;
+
+/**
+ * @author : hewei
+ * @date : 2018/1/8
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
